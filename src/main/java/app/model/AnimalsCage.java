d239f5572e97d52a307a95832682ca4121a9fda1
xxxradoxxx <trustus@inbox.ru>
2022-08-16 06:49:28 +0300

    fix timer

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -18,7 +18,7 @@ public class AnimalsCage {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 
